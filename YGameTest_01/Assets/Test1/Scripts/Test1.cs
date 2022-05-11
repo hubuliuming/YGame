b600f0fcadc0ae276f2b678afcb04707e93c91f8
Y <97585120+hubuliuming@users.noreply.github.com>
2022-05-11 17:02:06 +0800

    0

---
+++ YGameTest_01/Assets/Test1/Scripts/Test1.cs
@@ -0,0 +1,36 @@
+/****************************************************
+    文件：Test1.cs
+    作者：Y
+    邮箱: 916111418@qq.com
+    日期：#CreateTime#
+    功能：Nothing
+*****************************************************/
+
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+public class Test1 : MonoBehaviour
+{
+    private float _curTime;
+
+    private TimeSpan _timeSpan;
+    
+    private void Start()
+    {
+        // Stopwatch t = new Stopwatch();
+        // t.Start();
+        // t.Stop();
+        // t.Elapsed 
+        
+        Debug.Log(DateTime.Now.ToString("yyyyMMddHHmmss"));
+        //Debug.Log(DateTime.Now.ToString());
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
